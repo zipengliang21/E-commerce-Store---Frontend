d33cd8de1ab6c578e7805ee006fcd45bd8eeeb32
Zipeng Liang <zipengliang21@gmail.com>
2025-02-11 16:50:40 -0800

    fix typo

--- src/views/store/ProductDetail.jsx
+++ src/views/store/ProductDetail.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import apiInstance from "../../utils/axios";
-import GetCurrentAddress from "../plugin/UserCountry,";
+import GetCurrentAddress from "../plugin/UserCountry";
 import UserData from "../plugin/UserData";
 import CardID from "../plugin/CardID";
 
