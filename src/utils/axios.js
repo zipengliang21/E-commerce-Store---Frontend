b0cd113f246cce9a752cdd502f2a2ac91b0cddff
Zipeng Liang <zipengliang21@gmail.com>
2025-03-10 17:04:12 -0700

    Change Local url to Production url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: "https://zipeng-ecommerce-api.up.railway.app/api/v1/",
   timeout: 5000,
 
   headers: {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://127.0.0.1:8000/api/v1/";
+export const API_BASE_URL = "https://zipeng-ecommerce-api.up.railway.app/api/v1/";
 export const PAYPAL_CLIENT_ID =
   "Aa1I9pi34CZKGkxyx1NcobGOoqMc_QsUByZU4bgbpiR3f8qIJQMXB_x2Wb577SVVpBF37F_Pl59de41C";
-export const SERVER_URL = "http://127.0.0.1:8000/";
+export const SERVER_URL = "https://zipeng-ecommerce-api.up.railway.app/";
